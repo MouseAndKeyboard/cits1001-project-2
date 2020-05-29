d00afb55670ad3e826fd6c6528cfc4c77b4e5e28
Michael N <a@b.c>
2020-05-29 16:13:43 +0800

    Improved autosolver to be less bad

--- CheckSolution.java
+++ CheckSolution.java
@@ -1,4 +1,5 @@
 
+
 /**
  * CheckSolution is a utility class which can check if
  * a board position in an Aquarium puzzle is a solution.
@@ -16,82 +17,61 @@ public class CheckSolution
      * Non-constructor for objects of class CheckSolution
      */
     private CheckSolution(){}
-
+    
     /*
      * AUTOSOLVER COMPONENT
-     * Returns the number of conflicts in a given puzzle
+     * Convert a list of lists into a list of ints based on sublist size
      */
-    public static int conflicts(Aquarium p) {
-        int conflicts = 0;
-
-        // Valid row counts
-        int[] rowCounts = rowCounts(p);
-        for (int row = 0; row < p.getSize(); ++row) {
-            if (p.getRowTotals()[row] != rowCounts[row])
-                conflicts++;
-        }
-
-        // Valid column counts
-        int[] colCounts = columnCounts(p);
-        for (int col = 0; col < p.getSize(); ++col) {
-            if (p.getColumnTotals()[col] != colCounts[col])
-                conflicts++;
-        }
-
-        // Is there water above a row which has either air or mixed?
-        for (int row = 0; row < p.getSize() - 1; ++row) {
-            Integer[] aquariumsRowFull = Arrays.stream(p.getAquariums()[row]).boxed().toArray( Integer[]::new );
-            Set<Integer> aquariumsRow =  new HashSet<Integer>(Arrays.asList(aquariumsRowFull));
-            for (int aquarium : aquariumsRow) {
-                int[] status = rowStatus(p, aquarium, row);
-                int[] nextStatus = rowStatus(p, aquarium, row + 1);
-                if(status[1] != 2 && nextStatus[1] == 3)
-                    conflicts++;
-            }
-        }
-
-        // Is there split water
-        for (int row = 0; row < p.getSize(); ++row) {
-            Integer[] aquariumsRowFull = Arrays.stream(p.getAquariums()[row]).boxed().toArray( Integer[]::new );
-            Set<Integer> aquariumsRow = new HashSet<Integer>(Arrays.asList(aquariumsRowFull));
-            for (int aquarium : aquariumsRow) {
-                int[] status = rowStatus(p, aquarium, row);
-                if (status[1] == 3)
-                    conflicts++;
-            }
-        }
-
-        return conflicts;
-    }
-
-    /*
-     * AUTOSOLVER COMPONENT
-     * Returns the change in the number of conflicts in the puzzle given a particular move
-     */
-    public static int conflictsDelta(Aquarium p, int waterToMoveRow, int waterToMoveCol, int destinationRow, int destinationCol) {            
-        int oldConflicts = conflicts(p);
-        p.leftClick(destinationRow, destinationCol);
-        p.leftClick(waterToMoveRow, waterToMoveCol);
-        int newConflicts = conflicts(p);
-        p.leftClick(destinationRow, destinationCol);
-        p.leftClick(waterToMoveRow, waterToMoveCol);
-
-        return newConflicts - oldConflicts;
+    public static ArrayList<Integer> condenseList(ArrayList<ArrayList<Integer>> list) {
+        ArrayList<Integer> condensed = new ArrayList<Integer>();
+        for (ArrayList<Integer> sublist : list)
+            condensed.add(sublist.size());
+            
+        return condensed;
     }
     
-    public static ArrayList<ArrayList<Integer>> subsetSums(int sumTarget, int[] values) {
+    /*
+     * AUTOSOLVER COMPONENT
+     * returns a partition of the row based on contiguous aquarium segments
+     */
+    public static ArrayList<ArrayList<Integer>> aquariumRowPartition(Aquarium p, int row) {
+        ArrayList<ArrayList<Integer>> partition = new ArrayList<ArrayList<Integer>>();
+        int[] aquariumRow = p.getAquariums()[row];
+        int previousAquarium = aquariumRow[0];
+        ArrayList<Integer> contiguousSegment = new ArrayList<Integer>();
+        for (int column = 0; column < p.getSize(); ++column) {
+            int aquarium = aquariumRow[column];
+            if (aquarium != previousAquarium) {
+                partition.add(contiguousSegment);
+                previousAquarium = aquarium;
+                contiguousSegment = new ArrayList<Integer>();
+            } 
+            contiguousSegment.add(column);
+            
+            if (column == p.getSize() - 1) {
+                partition.add(contiguousSegment);
+            }
+        }
+        return partition;
+    }
+    
+    /*
+     * AUTOSOLVER COMPONENT
+     * returns all subsets from values which sum to sumTarget
+     */
+    public static ArrayList<ArrayList<Integer>> subsetSums(int sumTarget, ArrayList<Integer> values) {
         ArrayList<ArrayList<Integer>> subsetSums = new ArrayList<ArrayList<Integer>>();
         
-        int maxComparison = (int)Math.pow(2, values.length);
+        int maxComparison = (int)Math.pow(2, values.size());
         // maxComparison looks like: 000000011111
         
         for (int i = 0; i < maxComparison; ++i) {
             int currentSet = i;
             int sum = 0;
             ArrayList<Integer> subset = new ArrayList<Integer>();
-            for (int counter = 0; counter < values.length; ++counter) {
+            for (int counter = 0; counter < values.size(); ++counter) {
                 if (((currentSet >> counter) & 1) == 1) {
-                    sum += values[counter];
+                    sum += values.get(counter);
                     subset.add(counter);
                 }
             }
@@ -101,75 +81,23 @@ public class CheckSolution
         return subsetSums;
     }
     
-    // based on: https://enacademic.com/dic.nsf/enwiki/962116
     /*
      * AUTOSOLVER COMPONENT
-     * (In theory) should return an aquarium in the solved state
+     * (In theory) should return an aquarium in the solved state [not completed]
      */
     public static Aquarium solve(Aquarium p, int maxSteps) {
         p.clear();
 
-        int total = 0;
-        for (int rowConstraint : p.getRowTotals())
-            total += rowConstraint;
-
-        // assign "total" number of water squares
         for (int row = 0; row < p.getSize(); ++row) {
-            for (int col = 0; col < p.getSize(); ++col) {
-                if (total > 0) {
-                    p.leftClick(row, col);
-                    total--;
-                } else {
-                    break;
-                }
+            ArrayList<ArrayList<Integer>> partition = aquariumRowPartition(p, row);
+            ArrayList<ArrayList<Integer>> subsetSums = subsetSums(p.getRowTotals()[row], condenseList(partition));
+            if (subsetSums.size() == 1) {
+                for (Integer index : subsetSums.get(0))
+                    for (Integer col : partition.get(index))
+                        p.leftClick(row, col);
             }
         }
-
-        // simplify the problem by pre-solving some rows
-        // todo....
-
-        // run the constraint solver
-        for (int i = 0; i < maxSteps; ++i){
-            if (isSolution(p).equals("\u2713\u2713\u2713")) return p;
-
-            // randomly get x,y of a conflicted water tile
-            // Conflicted tiles are tiles which when removed would reduce the total number of conflicts
-            ArrayList<int[]> conflictingTiles = new ArrayList<>();   
-            for (int row = 0; row < p.getSize(); ++row) {
-                for (int col = 0; col < p.getSize(); ++col) {
-                    if (p.getSpaces()[row][col] == Space.WATER) {
-                        int[] position = {row, col};
-                        conflictingTiles.add(position);
-                    }
-                }
-            }
-
-            int range = conflictingTiles.size();
-
-            int[] randomConflictedTile = new int[2];
-
-            randomConflictedTile = conflictingTiles.get((int)(Math.random() * range));
-
-            int minDeltaRow = -1;
-            int minDeltaCol = -1;
-            int minDelta = Integer.MAX_VALUE;
-            for (int newRow = 0; newRow < p.getSize(); ++newRow) {
-                for (int newCol = 0; newCol < p.getSize(); ++newCol) {
-                    if (p.getSpaces()[newRow][newCol] != Space.WATER) {
-                        int conflictsDelta = conflictsDelta(p, randomConflictedTile[0], randomConflictedTile[1], newRow, newCol);
-                        if (conflictsDelta < minDelta) {
-                            // Not considering the circumstance where there are multiple moves 
-                            //     which are both minDelta... (perhaps a TODO?)
-                            minDelta = conflictsDelta;
-                            minDeltaRow = newRow;
-                            minDeltaCol = newCol;
-                        }
-                    }
-                }
-            }
-            p.leftClick(randomConflictedTile[0], randomConflictedTile[1]);
-            p.leftClick(minDeltaRow, minDeltaCol);
-        }
+        
         return p;
     }
 
